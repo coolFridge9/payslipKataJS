49c1d7fdfdfcb7b07884a1f2a54325eab0a7f736
jordan elley <jordan.elley@myob.com>
2018-09-27 23:31:04 +1200

    wrote test description

--- test/incomeTaxCalculator.test.js
+++ test/incomeTaxCalculator.test.js
@@ -10,7 +10,7 @@ ${18405}               | ${18238.95}    | ${"second tax bracket middle bound"}
 ${37000}               | ${21772}       | ${"second tax bracket upper bound"}
 ${60050}               | ${922}         | ${"middle case"}
 
-`("readWord function should", ({salary, expected, testReason}) => {
+`("calculate tax function should work for ", ({salary, expected, testReason}) => {
     it(testReason, () => {
         const result = calculateTax(salary);
         expect(result).toBe(expected);
