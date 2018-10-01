084171e838d0c4bf7abbc430c420dc773ac851ea
jordan elley <jordan.elley@myob.com>
2018-10-01 16:22:35 +1300

    updated and added more tests and modified code to pass tests

--- incomeTaxCalculator.js
+++ incomeTaxCalculator.js
@@ -4,12 +4,22 @@ exports.calculateTax = function(salary){
     } else if(salary < 37001){
         const dollarsOverTaxBracket = salary - 18200;
         const extraTax = dollarsOverTaxBracket * 0.19;
-        const fixedTax = 18200;
-        return fixedTax + extraTax;
+        const fixedTax = 0;
+        return extraTax + fixedTax;
     } else if(salary < 87001){
         const dollarsOverTaxBracket = salary - 37000;
-        const extraTax = dollarsOverTaxBracket * 0.325;
-        const fixedTax = 37000;
+        let extraTax = dollarsOverTaxBracket * 0.325;
+        const fixedTax = 3572;
+        return fixedTax + extraTax;
+    } else if(salary < 180001){
+        const dollarsOverTaxBracket = salary - 87000;
+        let extraTax = dollarsOverTaxBracket * 0.37;
+        const fixedTax = 19822;
+        return fixedTax + extraTax;
+    } else{
+        const dollarsOverTaxBracket = salary - 180000;
+        let extraTax = dollarsOverTaxBracket * 0.45;
+        const fixedTax = 54232;
         return fixedTax + extraTax;
     }
 };
