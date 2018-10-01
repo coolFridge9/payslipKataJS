8776d349f7f930e1ad8f315e021dd97d1cda3372
jordan elley <jordan.elley@myob.com>
2018-10-01 16:48:18 +1300

    refactored calculate tax function to reduce code reuse

--- incomeTaxCalculator.js
+++ incomeTaxCalculator.js
@@ -1,25 +1,20 @@
+function taxForBracket(salary, previousTaxBracketMaxIncome, centsPerDollarOverFixedTax, fixedTaxForBracket) {
+    const dollarsOverTaxBracket = salary - previousTaxBracketMaxIncome;
+    const extraTax = dollarsOverTaxBracket * centsPerDollarOverFixedTax;
+    const fixedTax = fixedTaxForBracket;
+    return extraTax + fixedTax;
+}
+
 exports.calculateTax = function(salary){
     if(salary < 18201){
         return 0;
     } else if(salary < 37001){
-        const dollarsOverTaxBracket = salary - 18200;
-        const extraTax = dollarsOverTaxBracket * 0.19;
-        const fixedTax = 0;
-        return extraTax + fixedTax;
+        return taxForBracket(salary, 18200, 0.19, 0);
     } else if(salary < 87001){
-        const dollarsOverTaxBracket = salary - 37000;
-        let extraTax = dollarsOverTaxBracket * 0.325;
-        const fixedTax = 3572;
-        return fixedTax + extraTax;
+        return taxForBracket(salary, 37000, 0.325, 3572);
     } else if(salary < 180001){
-        const dollarsOverTaxBracket = salary - 87000;
-        let extraTax = dollarsOverTaxBracket * 0.37;
-        const fixedTax = 19822;
-        return fixedTax + extraTax;
+        return taxForBracket(salary, 87000, 0.37, 19822);
     } else{
-        const dollarsOverTaxBracket = salary - 180000;
-        let extraTax = dollarsOverTaxBracket * 0.45;
-        const fixedTax = 54232;
-        return fixedTax + extraTax;
+        return taxForBracket(salary, 180000, 0.45, 54232);
     }
 };
