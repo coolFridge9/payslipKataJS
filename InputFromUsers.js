5c94979b9429c1a430de012669aee44afd46ee11
jordan elley <jordan.elley@myob.com>
2018-10-04 22:07:05 +1300

    added a validtion loop to the number user inputs to validate input

--- InputFromUsers.js
+++ InputFromUsers.js
@@ -1,11 +1,24 @@
-const readline = require('readline-sync');
+const readline = require("readline-sync");
+const validator = require("/Users/jordan.elley/code/payslipJS/numberValidators.js");
 
 
 exports.getUserInput = function() {
     const name = readline.question("Please input your name: ");
     const surname = readline.question("Please input your surname: ");
-    const annualSalary = readline.question("Please enter your annual salary: ");
-    const superRate = readline.question("Please enter your super rate: ");
+
+    let annualSalary = readline.question("Please enter your annual salary: ");
+    while(!validator.isPositiveNumber(annualSalary)) {
+        console.log("error invalid value");
+        annualSalary = readline.question("Please enter your annual salary: ");
+    }
+
+    let superRate = readline.question("Please enter your super rate: ");
+
+    while(!validator.isValidPercentage(superRate)) {
+        console.log("error invalid value");
+        superRate = readline.question("Please enter your super rate: ");
+    }
+
     const paymentStartDate = readline.question("Please input your payment start date: ");
     const paymentEndDate = readline.question("Please input your payment end date: ");
 
