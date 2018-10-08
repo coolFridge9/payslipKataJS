7fcffd3ff8162800705c44b95500c5ebf0d79a5f
jordan elley <jordan.elley@myob.com>
2018-10-08 14:11:21 +1300

    fixed bug by adding correct varaible to super and net income outputs

--- src/DisplayData.js
+++ src/DisplayData.js
@@ -3,8 +3,8 @@ exports.displayDataInText = function(processedUserData){
     console.log("Pay Period:", processedUserData.payPeriod);
     console.log("Gross Income:", processedUserData.grossIncome);
     console.log("Income Tax:", processedUserData.tax);
-    console.log("Net Income:", processedUserData.tax);
-    console.log("Super:", processedUserData.tax);
+    console.log("Net Income:", processedUserData.netIncome);
+    console.log("Super:", processedUserData.superContribution);
 
 
     console.log("\n" + "Thank you for using MYOB!");
