bcb790e08aaf726700038ff765eb163341e9b8d0
jordan elley <jordan.elley@myob.com>
2018-10-08 13:02:08 +1300

    fixed processUserDetails to be monthly and pass tests

--- processUserDetails.js
+++ processUserDetails.js
@@ -3,16 +3,16 @@ const calculateTax = require("/Users/jordan.elley/code/payslipJS/calculateTax.js
 exports.processUserDetails = function(userDetails){
     const name = processName(userDetails.firstName, userDetails.lastName);
     const payPeriod = getPayPeriod(userDetails.paymentStartDate, userDetails.paymentEndDate);
-    const grossIncome = calculateGrossIncome(userDetails.annualSalary);
-    const tax = calculateTax(userDetails.annualSalary);
-    const netIncome = calculateNetIncome(grossIncome, tax);
-    const superContribution = getSuper(netIncome, userDetails.superRate);
+    const grossIncome = calculateGrossMonthlyIncome(userDetails.annualSalary);
+    const monthlyTax = calculateMonthlyTax(userDetails.annualSalary);
+    const netIncome = calculateMonthlyNetIncome(grossIncome, monthlyTax);
+    const superContribution = getMonthlySuper(grossIncome, userDetails.superRate);
 
     return {
         name : name,
         payPeriod : payPeriod,
         grossIncome : grossIncome,
-        tax : tax,
+        tax : monthlyTax,
         netIncome : netIncome,
         superContribution : superContribution
     };
@@ -22,19 +22,31 @@ function processName(firstName, lastName){
     return firstName + " " + lastName;
 }
 
-function getPayPeriod(startDate, endDate){
-    return startDate + " - " + endDate;
+function padDate(date) {
+    const isNumber = require("./numberValidators").isPositiveNumber;
+    if( isNumber(date[0]) && !isNumber(date[1]) ){
+        date = "0" + date;
+    }
+    return date;
 }
 
-function calculateGrossIncome(annualSalary){
+function getPayPeriod(startDate, endDate){
+    return padDate(startDate) + " - " + padDate(endDate);
+}
+
+function calculateMonthlyTax(annualSalary){
+    return Math.round(calculateTax(annualSalary)/12);
+}
+
+function calculateGrossMonthlyIncome(annualSalary){
     return Math.round(annualSalary/12.0);
 }
 
-function calculateNetIncome(grossIncome, incomeTax){
-    return Math.round(grossIncome - (incomeTax / 12));
+function calculateMonthlyNetIncome(grossIncome, incomeTax){
+    return Math.round(grossIncome - (incomeTax));
 }
 
-function getSuper(netIncome, superRate) {
-    return Math.round(netIncome * superRate/100);
+function getMonthlySuper(grossIncome, superRate) {
+    return Math.round(grossIncome * superRate/100);
 }
 
